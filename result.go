6492a4e47b23c22680aac7c68ce286cf8721ad9f
Eli Sarver <elis@metrumrg.com>
2021-06-24 17:31:08 -0400

    Add optionality to environment setup
    - Add a New() function that optionally sets Env and Dir.
    - Rename Result to Capture Capture
    - Hang what was Capture() off of the Capture struct as Run()
    - rename Capture on result to Rerun on Capture, which only takes context.

--- result.go
+++ result.go
@@ -1,73 +1,128 @@
-// Package command provides an organized way of running a *exec.Cmd and generates a Result that contains all
-// commonly-accessed information in the Cmd that allows for repeated execution.
+// Package command provides an organized way of running a *exec.Cmd and generates
+// a Capture that contains all commonly-accessed information in the Cmd that allows
+// for repeated execution.
 package command
 
 import (
 	"context"
 	"errors"
 	"os/exec"
-	"path"
 )
 
-// Result represents a single execution of a *exec.Cmd.
-type Result struct {
-	// Name is the command to run, similar to exec.Command's first parameter
+// Capture represents the state for a run of a command.
+type Capture struct {
+	// Name is the name to run.
+	//
+	// This is the only field that must be set to a non-zero
+	// value. If Path is relative, it is evaluated relative
+	// to Dir.
 	Name string `json:"name"`
 
-	// Args contains the command line called, without argv[0].
+	// Args holds name line arguments, excluding the name as Args[0].
+	// This differs in behavior from the exec.Cmd type.
 	Args []string `json:"args,omitempty"`
 
-	// Env contains the environment passed into CaptureContext.
+	// Dir specifies the working directory of the name.
+	// If Dir is the empty string, Comand runs the name in the
+	// calling process's current directory.
+	Dir string `json:"working_dir,omitempty"`
+
+	// Env specifies the environment of the process.
+	// Each entry is of the form "key=value".
+	// If Env is nil, the new process uses the current process's
+	// environment.
+	// If Env contains duplicate environment keys, only the last
+	// value in the slice for each duplicate key is used.
+	// As a special case on Windows, SYSTEMROOT is always added if
+	// missing and not explicitly set to the empty string.
 	Env []string `json:"env,omitempty"`
 
-	// Output provides the combined output from the command as a string.
+	// Output provides the combined output from the name as a string.
 	Output string `json:"output,omitempty"`
 
 	// ExitCode holds the exit code returned by the call.
-	// It will be 0 (default value) even if a command didn't run due to error.
+	// It will be 0 (default value) even if a name didn't run due to error.
 	// You MUST check error when calling any of the functions below, as the
 	// exec.ExitError type contains additional context for failure.
 	ExitCode int `json:"exitCode"`
 }
 
-// CaptureContext executes an exec.CommandContext and returns Result and error.
+// Option is a value setters type given to the New function to set the optional parts of configuration.
+// This allows us to add some of the other exec.Cmd fields later if we have to.
+type Option func(*Capture)
+
+// WithEnv is passed into New() to set the environment.
+func WithEnv(env []string) Option {
+	return func(r *Capture) {
+		r.Env = env
+	}
+}
+
+// WithDir is passed into New() to set the working directory.
+func WithDir(dir string) Option {
+	return func(r *Capture) {
+		r.Dir = dir
+	}
+}
+
+// New creates a Capture struct with (optionally) Env and Dir set on it.
+func New(options ...Option) Capture {
+	var c Capture
+	for _, option := range options {
+		option(&c)
+	}
+
+	return c
+}
+
+// Run executes a name and returns Capture and error.
 //
-// The Result will always be returned, even if it's incomplete.
+// The Capture will always be returned, even if it's incomplete.
 //
 // The error type is passed on unwrapped and may contain an *exec.ExitError, which can be converted with errors.As
 // to check for additional information.
 //
 // The parameters behave as they do in *exec.Cmd, so passing in a nil env will inherit the parent environment,
 // and passing an empty slice will create and empty environment.
-func CaptureContext(ctx context.Context, env []string, name string, args ...string) (cr Result, err error) {
+func (c Capture) Run(ctx context.Context, name string, args ...string) (Capture, error) {
 	if ctx == nil {
 		ctx = context.Background()
 	}
 
-	cmd := exec.CommandContext(ctx, name, args...)
-	cmd.Env = env
-
-	return capture(cmd)
-}
-
-// Capture executes an exec.CommandContext and captures the output as a Result and an error. It has no control over
-// the context.Context. If you need to inject context.Context, use CaptureContext.
-func Capture(env []string, name string, args ...string) (cr Result, err error) {
-	return CaptureContext(context.Background(), env, name, args...)
-}
-
-func capture(cmd *exec.Cmd) (cr Result, err error) {
-	output, err := cmd.CombinedOutput()
-
-	cr = Result{
-		Name:     path.Base(cmd.Path),
-		Args:     cmd.Args[1:],
-		Env:      cmd.Env,
-		Output:   string(output),
-		ExitCode: errToExitCode(err),
+	if len(name) == 0 {
+		return c, errors.New("name empty")
 	}
 
-	return cr, err
+	c.Name = name
+	c.Args = args
+
+	cmd := c.makeExecCmd(ctx)
+	err := c.doCapture(cmd)
+
+	return c, err
+}
+
+// Rerun re-runs the Capture's parameters in a new shell, recording
+// A result to it in the same way as Run.
+func (c Capture) Rerun(ctx context.Context) (Capture, error) {
+	return c.Run(ctx, c.Name, c.Args...)
+}
+
+func (c Capture) makeExecCmd(ctx context.Context) *exec.Cmd {
+	cmd := exec.CommandContext(ctx, c.Name, c.Args...)
+	cmd.Env = c.Env
+	cmd.Dir = c.Dir
+
+	return cmd
+}
+
+func (c *Capture) doCapture(cmd *exec.Cmd) (err error) {
+	output, err := cmd.CombinedOutput()
+
+	c.Output = string(output)
+	c.ExitCode = errToExitCode(err)
+
+	return err
 }
 
 // errToExitCode converts potential errors to a nil-able int error code.
@@ -82,23 +137,4 @@ func errToExitCode(err error) int {
 	}
 
 	return 0
-}
-
-// CaptureContext re-runs the Result's parameters in a new shell, recording
-// A result in the same way as CaptureContext.
-func (cr Result) CaptureContext(ctx context.Context) (Result, error) {
-	if ctx == nil {
-		ctx = context.Background()
-	}
-
-	// suppress:G204
-	cmd := exec.CommandContext(ctx, cr.Name, cr.Args...)
-	cmd.Env = cr.Env
-
-	return capture(cmd)
-}
-
-// Capture is the same as CaptureContext without regard for controlling context.Context.
-func (cr Result) Capture() (Result, error) {
-	return cr.CaptureContext(context.Background())
 }
