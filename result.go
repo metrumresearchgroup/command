8b957f87a03236feed971fa53b0fd2ea234b9d7c
Eli Sarver <elis@metrumrg.com>
2021-06-24 12:26:06 -0400

    Additional documentation

--- result.go
+++ result.go
@@ -1,3 +1,5 @@
+// Package command provides an organized way of running a *exec.Cmd and generates a Result that contains all
+// commonly-accessed information in the Cmd that allows for repeated execution.
 package command
 
 import (
@@ -7,19 +9,32 @@ import (
 	"path"
 )
 
-// Result represents a single executable Diff. Output is combined output. If ExitError has a value, it will have
-// ExitError.Stderr embedded for stderr output.
+// Result represents a single execution of a *exec.Cmd
 type Result struct {
-	Name     string   `json:"name"`
-	Args     []string `json:"args,omitempty"`
-	Env      []string `json:"env,omitempty"`
-	Output   string   `json:"output,omitempty"`
-	ExitCode int      `json:"exitCode"`
+	// Name is the command to run, similar to exec.Command's first parameter
+	Name string `json:"name"`
+
+	// Args contains the command line called, without argv[0].
+	Args []string `json:"args,omitempty"`
+
+	// Env contains the environment passed into CaptureContext
+	Env []string `json:"env,omitempty"`
+
+	//
+	Output string `json:"output,omitempty"`
+
+	ExitCode int `json:"exitCode"`
 }
 
-// CaptureContext executes an exec.CommandContext and captures the output as a Result and an error.
+// CaptureContext executes an exec.CommandContext and returns Result and error.
+//
 // The Result will always be returned, even if it's incomplete.
-// The Result is re-runnable as seen in Result.Capture() and Result.CaptureContext()
+//
+// The error type is passed on unwrapped and may contain an *exec.ExitError, which can be converted with errors.As
+// to check for additional information.
+//
+// The parameters behave as they do in *exec.Cmd, so passing in a nil env will inherit the parent environment,
+// and passing an empty slice will create and empty environment.
 func CaptureContext(ctx context.Context, env []string, name string, args ...string) (cr Result, err error) {
 	if ctx == nil {
 		ctx = context.Background()
@@ -31,7 +46,8 @@ func CaptureContext(ctx context.Context, env []string, name string, args ...stri
 	return capture(cmd)
 }
 
-// Capture is the same as CaptureContext without caring about Contexts or timeouts.
+// Capture executes an exec.CommandContext and captures the output as a Result and an error. It has no control over
+// the context.Context. If you need to inject context.Context, use CaptureContext
 func Capture(env []string, name string, args ...string) (cr Result, err error) {
 	return CaptureContext(context.Background(), env, name, args...)
 }
@@ -65,7 +81,7 @@ func errToExitCode(err error) int {
 }
 
 // CaptureContext re-runs the Result's parameters in a new shell, recording
-// A result in the same way as command.CaptureContext.
+// A result in the same way as CaptureContext.
 func (cr Result) CaptureContext(ctx context.Context) (Result, error) {
 	if ctx == nil {
 		ctx = context.Background()
@@ -78,7 +94,7 @@ func (cr Result) CaptureContext(ctx context.Context) (Result, error) {
 	return capture(cmd)
 }
 
-// Capture is the same as CaptureContext without caring about Contexts or timeouts.
+// Capture is the same as CaptureContext without regard for controlling context.Context.
 func (cr Result) Capture() (Result, error) {
 	return cr.CaptureContext(context.Background())
 }
