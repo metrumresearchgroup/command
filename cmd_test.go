4895ac2463c371ad88446fb06354ddb102c262bd
Eli Sarver <elis@metrumrg.com>
2021-09-28 11:48:26 -0400

    add Impersonate function

--- cmd_test.go
+++ cmd_test.go
@@ -2,6 +2,12 @@ package command
 
 import (
 	"context"
+	"errors"
+	"os"
+	"os/exec"
+	"os/user"
+	"strconv"
+	"syscall"
 	"testing"
 	"time"
 
@@ -69,6 +75,127 @@ func TestCmd_KillAfter(tt *testing.T) {
 			t.R.NoError(c.Start())
 
 			t.R.NoError(<-errCh)
+		})
+	}
+}
+
+func TestCmd_Impersonate(tt *testing.T) {
+	type fields struct {
+		Cmd        *exec.Cmd
+		cancelFunc func()
+	}
+	type args struct {
+		username string
+		setPgid  bool
+	}
+	tests := []struct {
+		name            string
+		fields          fields
+		args            args
+		wantErr         bool
+		wantEnv         []string
+		wantSysProcAttr *syscall.SysProcAttr
+	}{
+		{
+			name: "garbage username",
+			fields: fields{
+				Cmd: exec.Command("dummy"),
+			},
+			args: args{
+				username: "asdfasdfsadf",
+				setPgid:  false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "empty username",
+			fields: fields{
+				Cmd: exec.Command("dummy"),
+			},
+			args: args{
+				username: "",
+				setPgid:  false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "assign self to test",
+			fields: fields{
+				Cmd: func() *exec.Cmd {
+					cmd := exec.Command("dummy")
+					cmd.Env = []string{"BASE=ALL"}
+
+					return cmd
+				}(),
+			},
+			args: args{
+				username: func() string {
+					u, err := user.Current()
+					if err != nil {
+						panic(err)
+					}
+					if len(u.Username) == 0 {
+						panic(errors.New("no user returned by user.Current()"))
+					}
+					return u.Username
+				}(),
+				setPgid: true,
+			},
+			wantErr: false,
+			wantEnv: func() []string {
+				home, err := os.UserHomeDir()
+				if err != nil {
+					panic(err)
+				}
+				u, err := user.Current()
+				un := u.Username
+				return []string{
+					"BASE=ALL",
+					"USER=" + un,
+					"HOME=" + home,
+				}
+			}(),
+			wantSysProcAttr: &syscall.SysProcAttr{
+				Setpgid: true,
+				Credential: func() *syscall.Credential {
+					u, err := user.Current()
+					if err != nil {
+						panic(err)
+					}
+					uid, err := strconv.Atoi(u.Uid)
+					if err != nil {
+						panic(err)
+					}
+					gid, err := strconv.Atoi(u.Gid)
+					if err != nil {
+						panic(err)
+					}
+					return &syscall.Credential{
+						Gid: uint32(gid),
+						Uid: uint32(uid),
+					}
+				}(),
+			},
+		},
+	}
+	for _, test := range tests {
+		tt.Run(test.name, func(tt *testing.T) {
+			t := wrapt.WrapT(tt)
+
+			c := &Cmd{
+				Cmd:        test.fields.Cmd,
+				cancelFunc: test.fields.cancelFunc,
+			}
+
+			err := c.Impersonate(test.args.username, test.args.setPgid)
+			t.R.WantError(test.wantErr, err)
+
+			if test.wantErr {
+				return
+			}
+
+			t.R.Equal(test.wantEnv, c.Env)
+			t.R.Equal(test.wantSysProcAttr, c.SysProcAttr)
 		})
 	}
 }
